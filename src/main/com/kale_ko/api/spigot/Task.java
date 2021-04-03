29a28c87bdc8a7b1babc39626e036b3862c610e3
Kale-Ko <info@kaleko.ga>
2021-04-03 16:17:30 -0400

    Update

---
+++ src/main/com/kale_ko/api/spigot/Task.java
@@ -0,0 +1,33 @@
+package com.kale_ko.api.spigot;
+
+import org.bukkit.Bukkit;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Task {
+    private static final Map<UUID, Integer> TASKS = new HashMap<>();
+    private final UUID uuid;
+
+    public Task(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setId(int id) {
+        TASKS.put(uuid, id);
+    }
+
+    public int getId() {
+        return TASKS.get(uuid);
+    }
+
+    public boolean hasId() {
+        return TASKS.containsKey(uuid);
+    }
+
+    public void stop() {
+        Bukkit.getScheduler().cancelTask(TASKS.get(uuid));
+        TASKS.remove(uuid);
+    }
+}
